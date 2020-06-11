16f4dd0df61270f0c89d293b34f656c2557e1128
Gershon Naidoo <63759690+Nukphonik@users.noreply.github.com>
2020-06-11 14:31:01 +0200

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace HelloWorld
+﻿namespace LunarLanding
 {
     partial class Form1
     {
@@ -28,34 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.picPlatform = new System.Windows.Forms.PictureBox();
+            this.panelScore = new System.Windows.Forms.Panel();
+            this.labelLx = new System.Windows.Forms.Label();
+            this.labelDy = new System.Windows.Forms.Label();
+            this.labelShips = new System.Windows.Forms.Label();
+            this.labelFuel = new System.Windows.Forms.Label();
+            this.picLander = new System.Windows.Forms.PictureBox();
+            this.MyPics = new System.Windows.Forms.ImageList(this.components);
+            this.labelScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlatform)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLander)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // timer1
             // 
-            this.button1.Location = new System.Drawing.Point(227, 198);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Hello World";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // picPlatform
+            // 
+            this.picPlatform.Location = new System.Drawing.Point(688, 13);
+            this.picPlatform.Name = "picPlatform";
+            this.picPlatform.Size = new System.Drawing.Size(100, 50);
+            this.picPlatform.TabIndex = 0;
+            this.picPlatform.TabStop = false;
+            this.picPlatform.Click += new System.EventHandler(this.PicPlatform_Click);
+            // 
+            // panelScore
+            // 
+            this.panelScore.Location = new System.Drawing.Point(107, 13);
+            this.panelScore.Name = "panelScore";
+            this.panelScore.Size = new System.Drawing.Size(509, 306);
+            this.panelScore.TabIndex = 1;
+            this.panelScore.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelScore_Paint);
+            // 
+            // labelLx
+            // 
+            this.labelLx.AutoSize = true;
+            this.labelLx.Location = new System.Drawing.Point(12, 26);
+            this.labelLx.Name = "labelLx";
+            this.labelLx.Size = new System.Drawing.Size(42, 13);
+            this.labelLx.TabIndex = 2;
+            this.labelLx.Text = "Label 1";
+            // 
+            // labelDy
+            // 
+            this.labelDy.AutoSize = true;
+            this.labelDy.Location = new System.Drawing.Point(12, 50);
+            this.labelDy.Name = "labelDy";
+            this.labelDy.Size = new System.Drawing.Size(0, 13);
+            this.labelDy.TabIndex = 3;
+            this.labelDy.Click += new System.EventHandler(this.LabelDy_Click);
+            // 
+            // labelShips
+            // 
+            this.labelShips.AutoSize = true;
+            this.labelShips.Location = new System.Drawing.Point(12, 72);
+            this.labelShips.Name = "labelShips";
+            this.labelShips.Size = new System.Drawing.Size(42, 13);
+            this.labelShips.TabIndex = 4;
+            this.labelShips.Text = "Label 3";
+            this.labelShips.Click += new System.EventHandler(this.LabelShips_Click);
+            // 
+            // labelFuel
+            // 
+            this.labelFuel.AutoSize = true;
+            this.labelFuel.Location = new System.Drawing.Point(12, 98);
+            this.labelFuel.Name = "labelFuel";
+            this.labelFuel.Size = new System.Drawing.Size(42, 13);
+            this.labelFuel.TabIndex = 5;
+            this.labelFuel.Text = "Label 4";
+            this.labelFuel.Click += new System.EventHandler(this.LabelFuel_Click);
+            // 
+            // picLander
+            // 
+            this.picLander.Location = new System.Drawing.Point(636, 205);
+            this.picLander.Name = "picLander";
+            this.picLander.Size = new System.Drawing.Size(100, 50);
+            this.picLander.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLander.TabIndex = 6;
+            this.picLander.TabStop = false;
+            this.picLander.Click += new System.EventHandler(this.PicLander_Click);
+            // 
+            // MyPics
+            // 
+            this.MyPics.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("MyPics.ImageStream")));
+            this.MyPics.TransparentColor = System.Drawing.Color.Transparent;
+            this.MyPics.Images.SetKeyName(0, "DarthShip.png");
+            this.MyPics.Images.SetKeyName(1, "DarthRight.png");
+            this.MyPics.Images.SetKeyName(2, "DarthLeft.png");
+            this.MyPics.Images.SetKeyName(3, "DarthUp.png");
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Location = new System.Drawing.Point(12, 120);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(42, 13);
+            this.labelScore.TabIndex = 7;
+            this.labelScore.Text = "Label 5";
+            this.labelScore.Click += new System.EventHandler(this.LabelScore_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelShips);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.labelLx);
+            this.Controls.Add(this.labelFuel);
+            this.Controls.Add(this.labelDy);
+            this.Controls.Add(this.picLander);
+            this.Controls.Add(this.panelScore);
+            this.Controls.Add(this.picPlatform);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.picPlatform)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLander)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox picPlatform;
+        private System.Windows.Forms.Panel panelScore;
+        private System.Windows.Forms.Label labelLx;
+        private System.Windows.Forms.Label labelDy;
+        private System.Windows.Forms.Label labelShips;
+        private System.Windows.Forms.Label labelFuel;
+        private System.Windows.Forms.PictureBox picLander;
+        private System.Windows.Forms.ImageList MyPics;
+        private System.Windows.Forms.Label labelScore;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,20 +8,267 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace HelloWorld
+namespace LunarLanding
 {
     public partial class Form1 : Form
     {
+        private double x, y;
+        private double Dx, Dy;
+        private int fuel = 100;
+        private int ships = 3;
+        private int score = 0;
         public Form1()
         {
             InitializeComponent();
+            initGame();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            if (MessageBox.Show("We're really sorry", "Will you forgive us ?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) ==  DialogResult.Yes)
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {/*end vertical if  */
+         //account for gravity
+            Dy += .0;
+            // increment score for being alive
+            score += 10000;
+            // show ordinary no flames lander
+            picLander.Image = MyPics.Images[0];
+            // call helper methods to handle details
+            moveShip();
+            CheckLanding();
+            ShowStats();
+        }// end tick timer
+
+        private void moveShip()
+        {
+            //change X and check for boundries
+            x += Dx;
+            if (x > this.Width - picLander.Width)
+            {
+                x = 0;
+            }
+            if (x < 0)
+            {
+                x = Convert.ToDouble(this.Width - picLander.Width);
+            }
+            //change in Y and check for boundries
+            y += Dy;
+            if (y > this.Height - picLander.Height)
+            {
+                y = 0;
+            }
+            if (y < 0)
+            {
+                y = Convert.ToDouble(this.Height - picLander.Height);
+            }
+            // move piclander to a new location
+            picLander.Location = new Point(Convert.ToInt32(x), Convert.ToInt32(y));
+        }
+
+        private void CheckLanding()
+        {
+            /* Get Rectangles from the objects */
+            Rectangle rLander = picLander.Bounds;
+            Rectangle rPlatform = picPlatform.Bounds;
+            // look for an intersection
+            if (rLander.IntersectsWith(rPlatform))
+            {
+                //
+                // its either a crash or a landing
+                // turn off the timer for a minute
+                timer1.Enabled = false;
+                if (Math.Abs(Dx) < 1)
                 {
+                    //horizontal speed ok
+                    if (Math.Abs(Dy) < 3)
+                    {
+                        //vertical speed ok
+                        if (Math.Abs(rLander.Bottom- rPlatform.Top) < 3)
+                        {
+                            // landing on top of platform
+                            MessageBox.Show("Good Job on the landing!!!!");
+                            fuel += 30;
+                            score += 10000;
+                        }
+                        else
+                        {
+                            //not on top of platoform
+                            MessageBox.Show("you have to land on top ");
+                            KillShip();
+                        }
+                    }// end on top if
+                    else
+                    { //Dy too large
+                        MessageBox.Show("too much vertical velocity");
+                        KillShip();
+                    }// end vertical if
                 }
+                else
+
+                {
+                    // if Dx too large
+                    MessageBox.Show("Too much horizontal velocity");
+                    KillShip();
+
+                }// end horizontal if
+                //reset the lander
+                initGame();
+            } // end if
+        } // end checking landing
+
+                
+        private void KillShip ()
+        {
+            // kill of a ship , check for end of game
+            DialogResult answer;
+            ships--;
+            if (ships <= 0)
+            {
+                // game is over
+                answer = MessageBox.Show("play again","Game Over", MessageBoxButtons.YesNo);
+                if (answer == DialogResult.Yes)
+                {
+                    ships = 3;
+                    fuel = 100;
+                    initGame();
+                }
+                else
+                {
+                    Application.Exit();
+                } // end if
+            } // end no ships if
+        }   // end kill ship
+
+        private void ShowStats()
+        {
+            // display the statistics
+            labelLx.Text = "dx: " + Dx;
+            labelDy.Text = "dy:" + Dy;
+            labelFuel.Text = "Fuel:" + fuel;
+            labelScore.Text = "Score:" + score;
+            labelShips.Text = "Ships:" + ships;
+        }// end show stats
+
+        private void initGame()
+        {
+            // re - initializes game
+            Random roller = new Random();
+            int platx, platy;
+            //find random dx and dy values for lander
+            Dx = Convert.ToDouble(roller.Next(5) - 2);
+            Dy = Convert.ToDouble(roller.Next(5) - 2);
+
+            // place lander randomly on  form
+            //  But make sure it appears
+            platx = roller.Next(this.Width - picPlatform.Width);
+            platy = roller.Next(this.Height - picPlatform.Height);
+            picPlatform.Location = new Point(platx, platy);
+            // turn on timer
+            timer1.Enabled = true;
+        }// end intigame
+        private void PicPlatform_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void PanelScore_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void LabelDy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LabelFuel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LabelShips_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PicLander_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LabelScore_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SplitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void SplitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+          
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // executes whenever user presses a key
+            // spend sond fuel
+            fuel--;
+            // check to see if user is out of fuel
+            if (fuel < 0)
+            {
+                timer1.Enabled = true;
+                MessageBox.Show("out of fuel");
+                fuel += 20;
+                KillShip();
+                initGame();
+            } // end if
+            // look for arrow keys
+            switch (e.KeyData)
+            {
+                case Keys.Up:
+                    if (timer1.Enabled == false)
+                    {
+                        timer1.Enabled = true;
+                    }
+                    picLander.Image = MyPics.Images[3];
+                    Dy -= 2;
+                    break;
+                case Keys.Left:
+                    if (timer1.Enabled == false)
+                    {
+                        timer1.Enabled = true;
+                    }
+
+                    picLander.Image = MyPics.Images[2];
+                    Dx++;
+                    break;
+                case Keys.Right:
+                    if (timer1.Enabled == false)
+                    {
+                        timer1.Enabled = true;
+                    }
+
+                    picLander.Image = MyPics.Images[1];
+                    Dx--;
+                    break;
+                case Keys.Down:
+                    picLander.Image = MyPics.Images[0];
+                    Dy += 2;
+                //    timer1.Enabled = false;
+                    
+                    break;
+                default:
+                    //do nothing
+                    break;
+            }//End  switch
+        } // end key down
+
         }
     }
-}
+
+
